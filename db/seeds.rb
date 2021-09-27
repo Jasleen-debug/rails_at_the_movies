8ad6abb6c22d39e428e927f54a0d34176d2ea7f5
Jasleen <jasleen0859@gmail.com>
2021-09-27 10:29:56 -0500

    created seeds.rb to populate our db

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,21 @@ movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 movies.each do |m|
   # puts m["original_title"]
   production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
-  if production_company && production_company.valid?
-    # create movie gere
+
+  if production_company&.valid?
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
   else
     puts "Invalid Production Company: #{m['production_company']} for movie: #{m['original_title']}"
   end
 end
+
+puts "Created #{ProductionCompany.count} Production Companies"
+
+puts "Created #{Movie.count} movies."
